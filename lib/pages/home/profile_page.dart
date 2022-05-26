ccd2e27cc87a0ce293988826450ce96b644b56d3
Aldi Jayadi <aldi.jayadi29@gmail.com>
2022-05-27 02:02:33 +0700

    update profile page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -5,10 +5,123 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Profile PAge'),
-      ),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(
+              defaultMargin,
+            ),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/images/image_avatar.png',
+                    width: 64,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hallo, Alex',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 24, fontWeight: semiBold),
+                    ),
+                    Text(
+                      '@alexkeinn',
+                      style: suibTitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                )),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, '/sign-in', (route) => false);
+                  },
+                  child: Image.asset(
+                    'assets/icons/button_logout.png',
+                    width: 20,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget menuItem(String text) {
+      return Container(
+        margin: EdgeInsets.only(top: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              text,
+              style: secondaryTextStyle.copyWith(
+                fontSize: 13,
+              ),
+            ),
+            Icon(
+              Icons.chevron_right,
+              color: primaryColor,
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          decoration: BoxDecoration(
+            color: backgroundColor3,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Account',
+                style: primaryTextStyle.copyWith(
+                    fontWeight: semiBold, fontSize: 16),
+              ),
+              menuItem('Edit Profile'),
+              menuItem('Your Orders'),
+              menuItem('Help'),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'General',
+                style: primaryTextStyle.copyWith(
+                    fontWeight: semiBold, fontSize: 16),
+              ),
+              menuItem('Privacy & Policy'),
+              menuItem('Term of Service'),
+              menuItem('Rate App'),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [header(), content()],
     );
   }
 }

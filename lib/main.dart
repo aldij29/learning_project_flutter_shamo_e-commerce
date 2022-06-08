93af176538058805c3d002dfdea6fcc615f76e1b
Aldi Jayadi <aldi.jayadi29@gmail.com>
2022-06-08 09:10:09 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, routes: {
-      '/': (context) => const SplashPage(),
-      '/sign-in': (context) => const SignInPage(),
-      '/sign-up': (context) => const SignUpPage(),
-      '/home': (context) => const MainPage(),
-      '/detail-chat': (context) => const DetailChatPage(),
-      '/edit-profile': (context) => const EditProfilePage(),
-      '/product': (context) => ProductPage(),
-      '/cart': (context) => const CartPage(),
-      '/checkout': (context) => const CheckoutPage(),
-      '/checkout-success': (context) => const CheckoutSuccessPage()
-    });
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/sign-in': (context) => const SignInPage(),
+        '/sign-up': (context) => const SignUpPage(),
+        '/home': (context) => const MainPage(),
+        '/detail-chat': (context) => const DetailChatPage(),
+        '/edit-profile': (context) => const EditProfilePage(),
+        '/product': (context) => ProductPage(),
+        '/cart': (context) => const CartPage(),
+        '/checkout': (context) => const CheckoutPage(),
+        '/checkout-success': (context) => const CheckoutSuccessPage()
+      },
+    );
   }
 }
